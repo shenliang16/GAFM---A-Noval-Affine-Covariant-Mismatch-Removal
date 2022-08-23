de5dcbdceabc730ab5549fa7c7a8680d5ff1ce57
shenliang16 <57088800+shenliang16@users.noreply.github.com>
2022-08-23 13:58:37 +0800

    Update LAF_to_pt3x3.m

--- GAFM/LAF_to_pt3x3.m
+++ GAFM/LAF_to_pt3x3.m
@@ -5,12 +5,7 @@ if isempty(Xf)
    return; 
 end
 
-temp = sum(Xf);
-if temp(3)>0.05*temp(5) 
-    Xp = Xf;
-    Yp = Yf;  
-    return;
-end
+
 affptx(:).x=Xf(:,5)';           affpty(:).x=Yf(:,5)';
 affptx(:).y=Xf(:,6)';           affpty(:).y=Yf(:,6)';
 affptx(:).a11=Xf(:,1)';         affpty(:).a11=Yf(:,1)';
@@ -19,7 +14,7 @@ affptx(:).a21=Xf(:,2)';         affpty(:).a21=Yf(:,2)';
 affptx(:).a22=Xf(:,4)';         affpty(:).a22=Yf(:,4)';
 
 X_3Pts=affpt_to_pt3x3(affptx);  Y_3Pts=affpt_to_pt3x3(affpty);
-% 将X和Y组合为一个矩阵用于输入：[Xpt1;Ypt1;Xpt2;Ypt2;Xpt3;Ypt3]
+% 灏哫鍜孻缁勫悎涓轰竴涓煩闃电敤浜庤緭鍏ワ細[Xpt1;Ypt1;Xpt2;Ypt2;Xpt3;Ypt3]
 Xp = X_3Pts([1:2,4:5,7:8],:)';   
 Yp = Y_3Pts([1:2,4:5,7:8],:)';  
 
